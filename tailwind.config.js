22ce4c6e7b5c71b9f267f3d8b2665a2b44394b48
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-04-22 18:09:27 -0700

    add: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: ['2rem', '3rem'],
+        lg: ['4rem', '5rem'],
+        xl: ['5rem', '6rem'],
+        '2xl': '7rem',
+      },
+      margin: {
+        DEFAULT: '1rem',
+        sm: ['2rem', '3rem'],
+        lg: ['4rem', '5rem'],
+        xl: ['5rem', '6rem'],
+        '2xl': '7rem',
+      },
+      // center: true, // Centering will disable any margin styles
     },
   },
-  plugins: [],
 }
