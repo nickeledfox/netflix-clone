fd0b4b644ae8abe6711e0fc4938ceac8bd7f278a
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-04-24 18:59:45 -0700

    add: api connection, requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,76 @@ import tw from 'twin.macro'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import { Header, Button } from '../components'
+import { Header, Hero, Button } from '../components'
 import { PageWrapper, Container } from '../styles'
+import requests from '../services/api/moviesReq'
+import { Movie } from '../types/typing'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home: React.FC<HomeProps> = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}) => {
   return (
     <PageWrapper>
       <Head>
         <title>Home | Netflix</title>
       </Head>
       <Header />
+      <Hero netflixOriginals={netflixOriginals} />
     </PageWrapper>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then(res => res.json()),
+    fetch(requests.fetchTrending).then(res => res.json()),
+    fetch(requests.fetchTopRated).then(res => res.json()),
+    fetch(requests.fetchActionMovies).then(res => res.json()),
+    fetch(requests.fetchComedyMovies).then(res => res.json()),
+    fetch(requests.fetchHorrorMovies).then(res => res.json()),
+    fetch(requests.fetchRomanceMovies).then(res => res.json()),
+    fetch(requests.fetchDocumentaries).then(res => res.json()),
+  ])
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
